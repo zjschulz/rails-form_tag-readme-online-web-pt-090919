60be1cc24ab3c893e61539770f3bb9914eeffb15
zjschulz <zjschulz@gmail.com>
2020-02-04 19:31:15 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,12 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
+  def new
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  class Application < Rails::Application
+    config.web_console.whitelisted_ips = '<IP address here>'
+  end
+  
 end
